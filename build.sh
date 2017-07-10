66786f76c1fb8ae22e3f7983120d0d9478838eb2
OJIMA Ryoji <rojima1@gmail.com>
2017-07-10 20:55:30 +0900

    some bugs are fixed.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-(cd base        && docker build -name rojima1/base        .)
-(cd clojure     && docker build -name rojima1/clojure     .)
-(cd python      && docker build -name rojima1/python      .)
-(cd base-cuda   && docker build -name rojima1/base-cuda   .)
-(cd python-cuda && docker build -name rojima1/python-cuda .)
+(cd base        && docker build -t rojima1/base        .)
+(cd clojure     && docker build -t rojima1/clojure     .)
+(cd python      && docker build -t rojima1/python      .)
+(cd base-cuda   && docker build -t rojima1/base-cuda   .)
+(cd python-cuda && docker build -t rojima1/python-cuda .)
 
 docker push rojima1/python
 docker push rojima1/python-cuda
