4c1584c7749e638c1e726a35ad7a0feb9f7b3609
OJIMA Ryoji <rojima1@gmail.com>
2017-07-11 21:48:40 +0900

    refactored.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-(cd base        && docker build -t rojima1/base        .) && \
-(cd clojure     && docker build -t rojima1/clojure     .) && \
-(cd python      && docker build -t rojima1/python      .) && docker push rojima1/python && \
-(cd node        && docker build -t rojima1/node        .) && \
+(cd base        && docker build --no-cache=true -t rojima1/base        .) && \
+(cd clojure     && docker build                 -t rojima1/clojure     .) && \
+(cd node        && docker build                 -t rojima1/node        .) && \
+(cd python      && docker build                 -t rojima1/python      .) && docker push rojima1/python && \
 # CUDA specified images
-(cd base-cuda   && docker build -t rojima1/base-cuda   .) && \
-(cd python-cuda && docker build -t rojima1/python-cuda .) && docker push rojima1/python-cuda
+(cd base-cuda   && docker build --no-cache=true -t rojima1/base-cuda   .) && \
+(cd python-cuda && docker build                 -t rojima1/python-cuda .) && docker push rojima1/python-cuda
