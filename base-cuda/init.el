a5e41b09bb8e3dca11084210279b48fbd1ef3416
OJIMA Ryoji <rojima1@gmail.com>
2017-07-12 08:33:24 +0900

    elpy setting is changed. using python3.

--- base-cuda/init.el
+++ base-cuda/init.el
@@ -89,7 +89,8 @@
 
 (defun init-elpy ()
   (elpy-enable)
-  (pyvenv-activate "~/Documents/Projects/python_ws"))
+  (setq elpy-rpc-python-command "python3")
+  (setq python-shell-interpreter "python3"))
 
 ;; markdown-modeを設定します。
 
